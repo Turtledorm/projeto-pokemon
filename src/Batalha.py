dda95fd4122c2fcb071326ac4b48dea74ce74592
gustavocw <gustavo.wandeur@usp.br>
2014-10-18 11:35:28 -0300

    Erros de codificação e clarificação da leitura

--- src/Batalha.py
+++ src/Batalha.py
@@ -1,63 +1,64 @@
 #!/usr/bin/python3
+# coding=utf-8
 
 from Pokemon import Pokemon
 
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
 def lePokemon():
     base = []
     atributos = []
 
-    # Lê o nome
-    base.append(input())
+    base.append(input("Nome: "))
+    base.append(int(input("Nível: ")))
 
-    # Lê o nível
-    base.append(int(input()))
-
+    print("Leitura de atributos:")
     for i in range(5):
-        lido = int(input())
-        atributos.append(lido)
+        atributo = int(input())
+        atributos.append(atributo)
 
+    print("Leitura de tipos:")
     for i in range(2):
-        lido = int(input())
-        if lido < 0 or lido > 16:
+        tipo = int(input())
+        if tipo not in range(16):
             erroLeitura("tipo do Pokémon")
-        base.append(lido)
+        base.append(tipo)
 
     pokemon = Pokemon(base, atributos)
     numAtaques = int(input())
 
+    print("Leitura de ataques:")
     for i in range(numAtaques):
         pokemon.adicionaAtaque(leAtaque())
 
     print("'" + base[0] + "' lido com sucesso!")
     return pokemon
 
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
 def leAtaque():
     ataque = []
 
-    # Lê o nome
-    ataque.append(input())
+    ataque.append(input("Nome: "))
 
-    typ = int(input())
-    if typ < 0 or typ > 16:
+    tipo = int(input())
+    if tipo not in range(16):
         erroLeitura("tipo de um ataque")
-    ataque.append(typ)
+    ataque.append(tipo)
 
     for i in range(3):
         ataque.append(int(input()))
 
     return ataque
 
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+
 
 def erroLeitura(mensagem):
     print("Erro ao ler " + mensagem + "!")
     exit(1)
 
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 # MAIN
 
 poke1 = lePokemon()
