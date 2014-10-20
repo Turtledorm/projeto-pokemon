a9b3320c0710e49a4780d5f8dfbfa57eb122cba6
nijek <nikolai.kotsifas@gmail.com>
2014-10-20 15:00:15 -0200

    Teste para pokemon.py funcionando

--- src/teste_pokemon.py
+++ src/teste_pokemon.py
@@ -2,44 +2,61 @@
 
 import unittest
 import random
-from pokemon import *
+import pokemon 
+from unittest.mock import *
 
 class PrimesTestCase(unittest.TestCase):
 
     def setUp(self):
         self.nome  = "Pedro" 
         self.lvl   = random.randint(0, 100)
-        self.hp    = random.randint(0, 100)
-        self.atk   = random.randint(0, 100)
-        self.defe  = random.randint(0, 100)
-        self.spd   = random.randint(0, 100)
-        self.spc   = random.randint(0, 100)
-        self.tipo1 = random.randint(0, 100)
-        self.tipo2 = random.randint(0, 100)
-        self.n_atk = random.randint(0, 100)
-
-        self.base = [self.nome, self.lvl, self.tipo1, self.tipo2]
-        self.atributos = [self.hp, self.atk, self.defe, self.spd, self.spc]
-        self.t = Pokemon(self.base, self.atributos)
+        self.hp    = random.randint(0, 255)
+        self.atk   = random.randint(0, 255)
+        self.dfs   = random.randint(0, 255)
+        self.spd   = random.randint(0, 255)
+        self.spc   = random.randint(0, 255)
+        self.tipo1 = random.randint(0, 12)
+        self.tipo2 = random.randint(0, 12)
+        
+        self.atk1 = ["n1", 1, 21, 10, 246]
+        self.atk2 = ["n2", 2, 22, 11, 247]
+        self.atk3 = ["n3", 3, 23, 12, 248]
+        self.atk4 = ["n4", 4, 24, 13, 249]
+       
+        self.entrada = [self.nome, self.lvl, self.hp, self.atk, 
+        self.dfs, self.spd, self.spc, self.tipo1, self.tipo2]
+        self.ataques = [4] #É o número de ataques que será parassado para o Pokemon.
+        self.ataques += self.atk1 + self.atk2 + self.atk3 + self.atk4
+        self.entrada += self.ataques #Todo o input está aqui agora.
+        
+        #Isso faz com que Pokemon receba a entrada do self.entrada e não do stdin.  
+        pokemon.input = Mock(side_effect = self.entrada)
+        self.t = pokemon.Pokemon()
 
     def test_pokemons(self):
         self.assertEqual(self.t.nome, self.nome)
         self.assertEqual(self.t.lvl, self.lvl)
         self.assertEqual(self.t.tipo1, self.tipo1)
         self.assertEqual(self.t.tipo2, self.tipo2)
-        self.assertEqual(self.t.hp, self.hp)
+        self.assertEqual(self.t.hp,  self.hp)
         self.assertEqual(self.t.atk, self.atk)
-        self.assertEqual(self.t.defe, self.defe)
+        self.assertEqual(self.t.dfs, self.dfs)
         self.assertEqual(self.t.spd, self.spd)
         self.assertEqual(self.t.spc, self.spc)
 
     def test_ataques(self):
-        self.t.adiciona_ataque(["n1", 1, 21, 10, 246])
-        self.t.adiciona_ataque(["n2", 2, 22, 11, 247])
-        self.t.adiciona_ataque(["n3", 3, 23, 12, 248])
-        self.t.adiciona_ataque(["n4", 4, 24, 13, 249])
-        with self.assertRaises(Exception):
-            self.t.adiciona_ataque(["n5", 5, 25, 14, 250])
+        self.ataques.pop(0)
+        """Cada 5 elementos da lista self.ataques correspondem aos atributos
+        de um ataque do pokemon. Então, o primeiro ataque do pokemon tem 
+        como atributos os valores de teste que estão no self.ataques[0] 
+        à self.ataques[4]""" 
+        for i in range(4):             
+            self.assertEqual(self.t.ataques[i].nome, self.ataques[5 * i])
+            self.assertEqual(self.t.ataques[i].typ,  self.ataques[5 * i + 1])
+            self.assertEqual(self.t.ataques[i].acu,  self.ataques[5 * i + 2])
+            self.assertEqual(self.t.ataques[i].pwr,  self.ataques[5 * i + 3])
+            self.assertEqual(self.t.ataques[i].pp ,  self.ataques[5 * i + 4])
+
 
 if __name__ == '__main__':
     unittest.main()
