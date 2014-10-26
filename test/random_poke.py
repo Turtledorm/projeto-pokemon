d51bf56fbb0631f562df726dc02be2eb5d313f62
nijek <nikolai.kotsifas@gmail.com>
2014-10-26 15:34:34 -0200

    teste_batalha finalizado. Modificação no random_poke, agora os ataques também são gerados aleatóriamente

--- test/random_poke.py
+++ test/random_poke.py
@@ -5,6 +5,14 @@ sys.path.insert(1, os.path.join(sys.path[0], '../src'))
 from pokemon import le_tipos, Ataque
 from string import ascii_lowercase, ascii_uppercase
 
+def gera_ataque():
+        return [''.join(choice(ascii_lowercase + ascii_uppercase)
+                        for x in range(randint(2, 10))),
+                randint(0, 16),
+                randint(1, 100),
+                randint(1, 100),
+                randint(1, 255)]
+
 class RandomPoke: 
     def __init__(self):
         
@@ -13,27 +21,26 @@ class RandomPoke:
     
         self._nome = ''.join(choice(ascii_lowercase + ascii_uppercase) for x in range(randint(2, 20)))
         self._lvl = randint(0, 100)
-        self._hp = randint(0, 255)
-        self._atk = randint(0, 255)
-        self._dfs = randint(0, 255)
+        self._hp = randint(10, 255)
+        self._atk = randint(1, 255)
+        self._dfs = randint(1, 255)
         self._spd = randint(0, 255)
-        self._spc = randint(0, 255)
+        self._spc = randint(1, 255)
         self._tipo1 = randint(0, 15)
         while True:
             self._tipo2 = randint(0, 16)
             if (self._tipo2 != self._tipo1):
                 break
     
-        self._ataques = [["n1", 1, 21, 10, 246],
-                        ["n2", 2, 22, 11, 247],
-                        ["n3", 3, 23, 12, 248],
-                        ["n4", 4, 24, 13, 249]]
+        self._ataques = [gera_ataque(),
+                         gera_ataque(),
+                         gera_ataque(),
+                         gera_ataque()]
     
         self.dados = [self._nome, self._lvl, self._hp, self._atk, self._dfs,
                  self._spd, self._spc, self._tipo1, self._tipo2,
                  [Ataque(self._ataques[i]) for i in range(4)]]
-                
-    
+
     def gera(self):
         """Retorna a lista que deve ser passada para pokemon.Pokemon 
         para criar um pokemon com os valores acima gerados."""
